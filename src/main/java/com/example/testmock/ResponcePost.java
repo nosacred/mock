5c02bd759afa8410b1f8c2cac16a183890fceae1
nosacred <und3rrrr@gmail.com>
2024-10-22 09:36:01 +0300

    v2

--- src/main/java/com/example/testmock/ResponcePost.java
+++ src/main/java/com/example/testmock/ResponcePost.java
@@ -8,19 +8,20 @@ import java.time.format.DateTimeFormatter;
 
 @JsonComponent
 public class ResponcePost {
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @JsonProperty("username")
     private String username;
     @JsonProperty("password")
     private String password;
     @JsonProperty("currentDate")
-    private String currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+    private  String currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
 
-
-//    @Override
-//    public String toString() {
-//        return "{\"username\":"+"\""+username+"\""+
-//                "\"password\":"+"\""+password+"\""+
-//                "\"currentDate\":"+"\""+currentDate+"\"}";
-//    }
 }
 
--- src/main/java/com/example/testmock/RestController.java
+++ src/main/java/com/example/testmock/RestController.java
@@ -1,28 +1,32 @@
 package com.example.testmock;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
+
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-
+    Random random = new Random();
 
     @RequestMapping("/get")
-    public String getMethod(@RequestParam(name = "login", required = false, defaultValue = "World") String login) throws InterruptedException {
-        Thread.sleep(1234);
-        return  "{\"username\": \"Alex\", \"password\": \"12345\", \"currentTime\": " +"\""+
-                LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"))+"\"}";
+    public @ResponseBody ResponcePost sendResponceGet(@RequestParam(name = "username", required = false, defaultValue = "Guest") String username,
+                                                      @RequestParam(name = "password", required = false, defaultValue = "Guest") String password) throws InterruptedException {
+
+        int delay =random.nextInt(1001)+1000;
+        ResponcePost responcePost= new ResponcePost();
+        responcePost.setUsername(username);
+        responcePost.setPassword(password);
+        Thread.sleep(delay);
+        return responcePost;
     }
 
+    @PostMapping(value = "/post", consumes = "Application/json")
+    public @ResponseBody ResponcePost sendResponcePost(@RequestBody ResponcePost responcePost) throws InterruptedException {
 
-    @PostMapping(value = "/post",produces = "Application/*", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public @ResponseBody ResponcePost getResponcePost(@RequestBody ResponcePost responcePost) throws InterruptedException {
-        Thread.sleep(1234);
+        int delay =random.nextInt(1001)+1000;
+        Thread.sleep(delay);
         return responcePost;
     }
 
