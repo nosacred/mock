b2be5323008fc2027fe328a0685723abee5dab37
nosacred <und3rrrr@gmail.com>
2024-10-22 16:13:19 +0300

    v3 add Valid for postMeth

--- src/main/java/com/example/testmock/RestController.java
+++ src/main/java/com/example/testmock/RestController.java
@@ -1,8 +1,15 @@
 package com.example.testmock;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import java.util.HashMap;
 import java.util.Random;
 
 
@@ -10,24 +17,40 @@ import java.util.Random;
 public class RestController {
     Random random = new Random();
 
-    @RequestMapping("/get")
-    public @ResponseBody ResponcePost sendResponceGet(@RequestParam(name = "username", required = false, defaultValue = "Guest") String username,
-                                                      @RequestParam(name = "password", required = false, defaultValue = "Guest") String password) throws InterruptedException {
+    @RequestMapping(value = "/get",method = RequestMethod.GET, produces = "application/json")
+    public  ResponseEntity<String> sendResponseGet() throws InterruptedException {
 
         int delay =random.nextInt(1001)+1000;
-        ResponcePost responcePost= new ResponcePost();
-        responcePost.setUsername(username);
-        responcePost.setPassword(password);
+        ResponceData responceData = new ResponceData("user","psww2424w");
         Thread.sleep(delay);
-        return responcePost;
+        return  ResponseEntity.status(200).body(responceData.toString());
     }
 
-    @PostMapping(value = "/post", consumes = "Application/json")
-    public @ResponseBody ResponcePost sendResponcePost(@RequestBody ResponcePost responcePost) throws InterruptedException {
+
+
+    @PostMapping(value = "post1", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> sendResponsePost(@RequestBody HashMap<String,String> data) throws InterruptedException {
+
+        if(data.size() !=2 ){
+            return ResponseEntity.status(400).body("Не корректный запрос!");
+        }
+        if (data.get("username").trim().isEmpty() || data.get("password").trim().isEmpty() ){
+            return ResponseEntity.status(400).body("Не корректный запрос! Пустые данные!");
+        }
 
         int delay =random.nextInt(1001)+1000;
         Thread.sleep(delay);
-        return responcePost;
+        ResponceData responceData = new ResponceData(data.get("username").trim(), data.get("password").trim());
+        return ResponseEntity.ok(responceData.toString());
+    }
+
+
+    @PostMapping(value = "post", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> sendResponsePost(@Valid @RequestBody ResponceData data) throws InterruptedException {
+
+        int delay =random.nextInt(1001)+1000;
+        Thread.sleep(delay);
+        return ResponseEntity.ok(data.toString());
     }
 
 }
